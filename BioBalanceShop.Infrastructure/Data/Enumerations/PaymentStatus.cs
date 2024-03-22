a1901582ff9062b634e71c20f12636aad56ce592
hrashkov <119432749+hrashkov@users.noreply.github.com>
2024-03-22 18:50:53 +0100

    Added Payment data entity and PaymentStatus enum

---
+++ BioBalanceShop.Infrastructure/Data/Enumerations/PaymentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioBalanceShop.Infrastructure.Data.Enumerations
+{
+    public enum PaymentStatus
+    {
+        Success = 1,
+        Failed = 2,
+        Cancelled = 3,
+        Credited = 4
+    }
+}
