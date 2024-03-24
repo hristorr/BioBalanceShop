461ace2541c1544e5fe3cb2f516bdc5ccbc067c5
hrashkov <119432749+hrashkov@users.noreply.github.com>
2024-03-24 19:59:57 +0100

    Renamed data configurations folder

---
+++ BioBalanceShop.Infrastructure/Data/Configurations/CustomerAddressConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using BioBalanceShop.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioBalanceShop.Infrastructure.Data.Configuration
+{
+    public class CustomerAddressConfiguration : IEntityTypeConfiguration<CustomerAddress>
+    {
+        public void Configure(EntityTypeBuilder<CustomerAddress> builder)
+        {
+            builder.HasQueryFilter(csa => csa.IsActive);
+
+            builder.HasOne(csa => csa.Country)
+                .WithMany(c => c.CustomerAddresses)
+                .HasForeignKey(c => c.CountryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            var data = new SeedData();
+
+            builder.HasData(new CustomerAddress[] {
+                data.IvanIvanovAddress
+            });
+        }
+    }
+}
---
+++ BioBalanceShop.Infrastructure/Data/Configurations/ShopConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BioBalanceShop.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioBalanceShop.Infrastructure.Data.Configuration
+{
+    public class ShopConfiguration : IEntityTypeConfiguration<Shop>
+    {
+        public void Configure(EntityTypeBuilder<Shop> builder)
+        {
+            builder.HasQueryFilter(s => s.IsActive);
+
+            var data = new SeedData();
+
+            builder.HasData(new Shop[] {
+                data.BioBalanceShop
+            });
+        }
+    }
+}
